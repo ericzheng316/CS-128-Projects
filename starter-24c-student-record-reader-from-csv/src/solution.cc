0a3d6299488db672ee6211fc4e5ba0af80a8115d
Boyu Zheng <boyuz5@illinois.edu>
2026-02-12 02:50:42 -0600

    new subtree-line-by-line-debugging

--- starter-24c-student-record-reader-from-csv/src/solution.cc
+++ starter-24c-student-record-reader-from-csv/src/solution.cc
@@ -1,6 +1,37 @@
 #include "solution.hpp"
 
+#include <fstream>
+
 Student ReadStudentRecFromStream(std::istream& is) {
+  Student stu;
   // your implementation here...
-  return Student{};
+  std::string first = "";
+  std::string last = "";
+  unsigned int uin = 0;
+  double gpa = 0.0;
+  char throwaway = '\0';
+
+  is >> first;
+  if (is.fail()) return Student{};
+
+  is >> last;
+  if (is.fail()) return Student{};
+
+  if (last.empty() || last.back() != ',') return Student{};
+
+  last.pop_back();
+
+  is >> uin;
+  if (is.fail()) return Student{};
+  is >> throwaway;
+  if (is.fail() || throwaway != ',') return Student{};
+
+  is >> gpa;
+  if (is.fail()) return Student{};
+
+  stu.full_name = first + " " + last;
+  stu.uin = uin;
+  stu.gpa = gpa;
+
+  return stu;
 }
